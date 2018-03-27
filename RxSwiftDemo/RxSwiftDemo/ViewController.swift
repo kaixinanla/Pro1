a357235458a6de6277696eb99f09f179483fe91e
盛嘉 <>
2018-03-27 18:09:58 +0800

    somethingDo

---
+++ RxSwiftDemo/RxSwiftDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RxSwiftDemo
+//
+//  Created by 盛嘉 on 2018/3/27.
+//  Copyright © 2018年 盛嘉. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var button1: UIButton!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor  = UIColor.red
+  }
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+
+
+}
+
