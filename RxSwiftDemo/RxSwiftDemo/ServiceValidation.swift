e9e26cba680e6c86c91e417257ac4f9a268dd8b4
盛嘉 <>
2018-03-28 19:21:46 +0800

    lalal

--- RxSwiftDemo/RxSwiftDemo/ServiceValidation.swift
+++ RxSwiftDemo/RxSwiftDemo/ServiceValidation.swift
@@ -26,10 +26,30 @@ class ValidationService {
       return .just(.failed(message: "号码长度至少6个字符"))
     }
     
-//    if usernameValid(username) {
-//      return .just(.failed(message: "账户已存在"))
-//    }
-    
     return .just(.ok(message: "用户名可用"))
+  }
+  
+  func validatePassword(_ password:String) -> Result {
+    if password.count == 0 {
+      return .empty
+    }
+    
+    if password.count < minCharactersCount {
+      return .failed(message: "密码长度至少6个字符")
+    }
+    
+    return .ok(message: "密码可用")
+  }
+  
+  func validateRepeatedPassword(_ password:String, _ repeatPassword:String) -> Result {
+    if repeatPassword.count == 0 {
+      return .empty
+    }
+    
+    if repeatPassword == password {
+      return .ok(message: "密码符合")
+    }
+    
+    return .failed(message: "两次密码不一致")
   }
 }
